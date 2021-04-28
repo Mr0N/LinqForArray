ba153c6d804e31509c38606c15cd97282ea34311
new_user <new_user@gmail.com>
2021-04-28 13:15:54 +0300

    Добавьте файлы проекта.

---
+++ LinqForArray/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqForArray
+{
+    static class XX
+    {
+        /// <summary>
+        /// Кільікість колонок
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int CountCol<R>(this R[,] array)
+        {
+            return array.GetLength(1);
+        }
+        /// <summary>
+        /// Кількість рядків
+        /// </summary>
+        /// <typeparam name="R"></typeparam>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int CountRow<R>(this R[,] array)
+        {
+            return array.GetLength(0);
+        }
+
+        public static IEnumerable<IEnumerable<T>> Select<T>(this T[,] array,
+                                Func<IEnumerable<T>, IEnumerable<T>> func)
+        {
+            int countRow = array.CountRow();
+            int count = 0;
+            if (countRow == 0) yield break;
+            while (countRow > count)
+            {
+                yield return func(GetCol(array, count));
+                count++;
+            }
+            IEnumerable<T> GetCol(T[,] arr, int index)
+            {
+                int countCol = arr.CountCol();
+                for (int i = 0; i < countCol; i++)
+                {
+                    yield return arr[index, i];
+                }
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var array =  new[,] { { 1, 2, 3 }, { 1, 2, 3 } };
+            var result = array.Select(a => a.Select(x=>x+3232));
+            foreach (var va in result)
+            {
+                Console.WriteLine("Begin");
+                foreach (var item in va)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
